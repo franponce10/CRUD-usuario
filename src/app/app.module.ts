922e71f515a754a81b141f5cd17e9e6abcb55f42
franponce10 <franseba1996@gmail.com>
2023-05-31 13:16:55 +0200

    Primer commit - creacion de componentes y servici

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListadoEmpleadosComponent } from './components/listado-empleados/listado-empleados.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListadoEmpleadosComponent,
+    LoginComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
