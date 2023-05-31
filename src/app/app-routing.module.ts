b0d2279d440789c4938c88c15cc6512408a07423
Fran Perez <fcoperezrey@gmail.com>
2023-05-31 14:26:30 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ListadoEmpleadosComponent } from './components/listado-empleados/listado-empleados.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'empleados', component: ListadoEmpleadosComponent},
+  {path: 'formulario', component: FormularioComponent}
 ];
 
 @NgModule({
