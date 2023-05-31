f2367939d580fbfc65674b56c55d0d4bc9cd6049
Fran Perez <fcoperezrey@gmail.com>
2023-05-31 14:14:22 +0200

    RouterLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { ListadoEmpleadosComponent } from './components/listado-empleados/listado-empleados.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'empleados', component: ListadoEmpleadosComponent},
 ];
 
 @NgModule({
